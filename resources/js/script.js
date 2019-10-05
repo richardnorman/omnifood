af1e17ff0dd80107d560cef030484f7e7c0274fa
Richard Norman <richard@webnova.ca>
2019-10-04 23:31:06 -0600

    Optimized site speed and added meta

--- resources/js/script.js
+++ resources/js/script.js
@@ -83,7 +83,23 @@ $(document).ready(function () {
     $('.js--wp-4').waypoint(function (direction) {
         $('.js--wp-4').addClass('animated pulse');
     },
-        {
-            offset: '60%'
-        });
+    {
+        offset: '60%'
+    });
+
+    /* mobile navi */
+    $('.js--nav-icon').click(function () {
+        var nav = $('.main-nav');
+        var icon = $('.js--nav-icon i');
+
+        // open navi animation
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
 });
